b1139bea18606dfe53e75c0cd0e3cd5b0945e685
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-11-23 21:11:59 +0300

    add oddAndEven

---
+++ oddAndEven/oddAndEven.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+if (num % 2 == 0)
+{
+Console.WriteLine("Это четное число"); 
+}
+else
+{
+Console.WriteLine("Это нечетное число");
+}
+Console.ReadKey();
