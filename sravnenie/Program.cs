e7e175b159f899814f88218b21f14d609922dbcb
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-11-23 20:05:52 +0300

    fix sravnenie

--- sravnenie/Program.cs
+++ sravnenie/Program.cs
@@ -1,18 +1,19 @@
-﻿Console.WriteLine("Введите первое число: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
- 
-            if(num1 > num2)
-            {
-                Console.Write(num1 + " больше " + num2);
-            }
-            else if (num1 < num2)
-            {
-                Console.Write(num1 + " меньше " + num2);
-            }
-            else
-            {
-                Console.WriteLine("Оба числа равны");
-            }
-            Console.ReadKey();
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write(num1 + " больше чем " + num2);
+}
+else if (num1 < num2)
+{
+    Console.Write(num1 + " меньше чем " + num2);
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+}
+Console.ReadKey();
